d8bc1751d5f0d8a6395e0c97a59f69c444606cc2
davidroman-hub <jobroman83@gmail.com>
2020-05-24 18:37:28 +0100

    quill modules, event handlers and localstorage blog

--- components/crud/Blog.js
+++ components/crud/Blog.js
@@ -7,12 +7,25 @@ import {withRouter} from 'next/router'
 import {getCookie, isAuth} from '../../actions/auth'
 import {getCategories} from '../../actions/category'
 import {getTags} from '../../actions/tags'
+import {QuillModules, QuillFormats} from '../../helpers/quill'
 const ReactQuill = dynamic(() => import('react-quill'), {ssr:false})
 import '../../node_modules/react-quill/dist/quill.snow.css'
 //import 'react-quill/dist/quill.bubble.css'
 const NewBlog = ({router}) => {
 
-    const [body, setBody] = useState({})
+    /// grab the blog from the localstorage
+    const blogFromLocalStorage = () => {
+        if(typeof window === 'undefined'){
+            return false
+        }
+        if(localStorage.getItem('blog')){
+            return JSON.parse(localStorage.getItem('blog'))
+        } else {
+            return false
+        }
+    }
+
+    const [body, setBody] = useState(blogFromLocalStorage())
     const [values, setValues] = useState({
         error:'',
         sizeError:'',
@@ -24,13 +37,24 @@ const NewBlog = ({router}) => {
 
     const {error, sizeError, success, formData, title, hidePublishButton} = values
 
+    useEffect(() => {
+        setValues({...values,formData: new FormData})
+    },[router])
 
     const handleChange = name => e => {
-        return console.log(e.target.value);
+        //return console.log(e.target.value);
+        const value = name === 'photo' ? e.target.files[0] : e.target.value
+        formData.set(name, value)
+        setValues({...values, [name]:value, formData, error:''})
     } 
 
-    const handleBody = name => e => {
-        return console.log(e.target.value);
+    const handleBody = e => {
+        //return console.log(e.target.value);
+        setBody(e)
+        formData.set('body', e)
+        if(typeof window !== 'undefined'){
+            localStorage.setItem('blog', JSON.stringify(e))
+        }
     } 
     
     const createFormBlog = () => {
@@ -47,7 +71,7 @@ const NewBlog = ({router}) => {
                     <input type="text" className="form-control" value={title} onChange={handleChange('title')}/>
                 </div>   
                 <div className='form-group'>
-                    <ReactQuill value={body} placeholder='Escribe algo increible' onChange={handleBody}/>
+                    <ReactQuill modules={QuillModules} formats={QuillFormats} value={body} placeholder='Escribe algo increible...' onChange={handleBody}/>
                 </div>
                 <div>
                     <button type='submit' className='btn btn-primary'>
@@ -64,9 +88,12 @@ const NewBlog = ({router}) => {
             
         {/* {JSON.stringify(router)} */}
         {createFormBlog()}
-
+        <hr/>
+        {JSON.stringify(title)}
         </div>
     )
 }
 
+
 export default withRouter(NewBlog)
+
