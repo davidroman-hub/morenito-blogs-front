aecd9b4709d6cb2b980b03e72fac2cd64b979295
davidroman-hub <jobroman83@gmail.com>
2020-05-26 00:15:36 +0100

    showing blogs in JSON

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -5,7 +5,7 @@ import {useState,useEffect} from 'react'
 import {listBlogsWithCategoriesAndTags} from '../../actions/blog'
 import {API} from '../../config'
 
-const Blogs = () => {
+const Blogs = ({blogs, categories, tags, size}) => {
     return (
        
             <Layout>
@@ -13,7 +13,7 @@ const Blogs = () => {
                     <div className="container-fluid">
                         <header>
                             <div className="col-md-12 pt-3">
-                                <h1 className="display-4 font-weight-bold text-center">Blogs sobre videojuegos, tutoriales y tecnología!</h1>
+                                <h4 className="display-4 font-weight-bold text-center">Blogs sobre videojuegos, tutoriales y tecnología!</h4>
                             </div>
                             <section className="">
                                 <p>Show categories and tags</p>
@@ -22,13 +22,28 @@ const Blogs = () => {
                     </div>
                     <div className="container-fluid">
                             <div className="row">
-                                <div className="col-md-12"> Show all blogs</div>
+                                 <div className="col-md-12"> {JSON.stringify(blogs)}</div>
                             </div>
                     </div>
                 </main>
             </Layout>
        
     )
+}
+
+Blogs.getInitialProps = () => {
+    return listBlogsWithCategoriesAndTags().then(data => {
+        if (data.error){
+            console.log(data.error)
+        } else {
+            return {
+                blogs: data.blogs,
+                categories: data.categories, 
+                tags: data.tags, 
+                size: data.size
+            }
+        }
+    } )
 }
 
 export default Blogs // getInitialProps
